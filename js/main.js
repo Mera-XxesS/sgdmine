eed9c5689cc29a7614f143ed229d9bdf4cf173f4
MAZUCA PEREZ LEONARDO <mazuca.leonardo@gmail.com>
2026-03-13 14:07:58 -0600

    commit colors

---
+++ js/main.js
@@ -0,0 +1,12 @@
+Swal.fire({
+  title: "<strong>CUIDADO</strong>",
+  icon: "info",
+  html: `
+    Este dispositivo pertenece a la <b><a href="#">06 JUNTA DISTRITAL EJECUTIVA DEL <strong>INE</strong> EN COAHUILA</a></b>,
+     puedes regresar el dispositovo en la siguiente direccion: 
+    C. BOULEVARD PEDRO RODRIGUEZ TRIANA, No.747, Int.33, Colonia PLAZA JUMBO, CP 27272
+    OFICINAS DISTRITALES 06 Referencia: PLAZA JUMBO Tel. 8717757575.
+  `,
+  focusConfirm: true,
+  confirmButtonText: `Confirmar!`
+});
